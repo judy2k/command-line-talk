58e10a720eba8f13e01b4e79a70a73ab398c6747
Mark Smith <mark.smith@practicalpoetry.co.uk>
2014-06-01 19:22:18 +0100

    Completed hello_worlds

---
+++ examples/hello_world5.py
@@ -0,0 +1,14 @@
+ap = ArgumentParser()
+ap.add_argument('-v', '--verbose',
+    default=False, action='store_true',
+    help='Increase verbosity')
+ap.add_argument('-n', '--number',
+    type=int, default=1,
+    help="The number of times to greet NAME")
+ap.add_argument('name', help="The person to greet")
+args = ap.parse_args()
+
+for index in range(args.number):
+    print "Hello,", args.name, "!"
+if args.verbose:
+    print "I've finished now."
